9b968bf3247619ac3576757778551fed12a899ba
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2020-01-06 11:18:55 +0900

    aaa

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,30 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.all.order("created_at DESC").page(params[:page]).per(5)
   end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    Message.create(message_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    message = Message.find(params[:id])
+    message.destroy
+  end
+
+  def show
+    @message = Message.find(params[:id])
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:title, :content, :image)
+  end
+
+
 end
