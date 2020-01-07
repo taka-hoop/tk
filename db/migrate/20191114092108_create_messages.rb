8b4f4bcc8ea2587a30ae690da63d90cf145b09a0
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2020-01-07 19:04:16 +0900

    test

--- db/migrate/20191114092108_create_messages.rb
+++ db/migrate/20191114092108_create_messages.rb
@@ -4,6 +4,8 @@ class CreateMessages < ActiveRecord::Migration[5.2]
       t.text :title
       t.text :content
       t.text :image
+      t.integer :area
+      t.integer :type
       t.timestamps
     end
   end
