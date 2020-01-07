b85aa2448519178a6e0688716ee7f505273cdce7
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2020-01-07 12:08:22 +0900

    footer

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.all.order("created_at DESC").page(params[:page]).per(5)
+    @messages = Message.all.order("created_at DESC").page(params[:page]).per(20)
   end
 
   def new
@@ -9,20 +9,6 @@ class MessagesController < ApplicationController
 
   def create
     Message.create(message_params)
-    redirect_to root_path
-  end
-
-  def show
-    @message = Message.find(params[:id])
-  end
-
-  def edit
-    @message = Message.find(params[:id])
-  end
-
-  def update
-    message = Message.find(params[:id])
-    message.update(post_params)
     redirect_to root_path
   end
 
