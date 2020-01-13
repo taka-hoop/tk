734d2cd9091362f341dcac4143ceee68f7f466f4
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2020-01-13 19:26:27 +0900

    tes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,7 +25,7 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:title, :content, :image, :area_id, :type_id)
+    params.require(:message).permit(:title, :content, :image, :area_id, :type_id).merge(user_id: current_user.id)
   end
 
 
