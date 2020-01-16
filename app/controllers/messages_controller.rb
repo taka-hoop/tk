195c4c110eb5528f0b75bdf14c30d63fb09aa009
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2020-01-16 20:28:10 +0900

    zz
    zz

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -31,7 +31,7 @@ class MessagesController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in?
+    redirect_to messages_path unless user_signed_in?
   end
 
 
